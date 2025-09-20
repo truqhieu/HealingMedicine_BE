70f7fb13aa8a02b2d988fa179ca0fadd2386fd1b
Thao1503 <beltatu01@gmail.com>
2025-09-20 13:10:41 +0700

    Revert "Manage user"

--- models/user.model.js
+++ models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['Patient', 'Doctor', 'Admin', 'Staff'],
+    enum: ['Patient', 'Doctor', 'Admin'],
     default: 'Patient'
   },
   status: {
