41b0f90148ae55fed8502d075569ee122889a03f
Thao <thaotvhe170862@fpt.edu.vn>
2025-10-23 02:16:40 +0700

    fix schedule

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { checkAvailableDoctors, createScheduleDoctor, getAllScheduleDoctors, deleteSchedule, getAllScheduleDoctors, updateScheduleDoctor } = require('../controllers/shedule.controller');
-const { viewDetailClinicRoom } = require('../controllers/clinic.controller');
+const { checkAvailableDoctors, createScheduleDoctor, getAllScheduleDoctors, deleteSchedule, updateScheduleDoctor, viewDetailScheduleDoctor } = require('../controllers/shedule.controller');
+const { verifyToken, verifyRole } = require('../middleware/auth.middleware');
 
-router.get('/schedules/doctor-available',verifyToken, verifyRole('Manager'), checkAvailableDoctors)
+router.get('/schedules/doctor-available', verifyToken, verifyRole('Manager'), checkAvailableDoctors)
 router.post('/schedules', verifyToken, verifyRole('Manager'),createScheduleDoctor)
 router.get('/schedules', verifyToken, verifyRole('Manager'),getAllScheduleDoctors)
-router.get('/schedules/:id', verifyToken, verifyRole('Manager'),viewDetailClinicRoom)
+router.get('/schedules/:id', verifyToken, verifyRole('Manager'),viewDetailScheduleDoctor)
 router.patch('/schedules/:id', verifyToken, verifyRole('Manager'),updateScheduleDoctor)
 router.delete('/schedules/:id',verifyToken, verifyRole('Manager'), deleteSchedule)
 
