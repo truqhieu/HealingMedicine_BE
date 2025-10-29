bf9f3c34e050f2048f42a639dd34e3e52f701879
truqhieu <trunghieu2003hhh@gmail.com>
2025-10-29 19:12:23 +0700

    Fix bug

--- routes/schedule.route.js
+++ routes/schedule.route.js
@@ -7,17 +7,19 @@ const { verifyToken, verifyRole } = require('../middleware/auth.middleware');
 router.get('/schedules/doctor-available', verifyToken, verifyRole('Manager'), checkAvailableDoctors)
 router.post('/schedules', verifyToken, verifyRole('Manager'),createScheduleDoctor)
 router.get('/schedules', verifyToken, verifyRole('Manager'),getAllScheduleDoctors)
-router.get('/schedules/:id', verifyToken, verifyRole('Manager'),viewDetailScheduleDoctor)
-router.patch('/schedules/:id', verifyToken, verifyRole('Manager'),updateScheduleDoctor)
-router.delete('/schedules/:id',verifyToken, verifyRole('Manager'), deleteSchedule)
+
+// New routes for doctor working hours management - MUST be before /schedules/:id
+router.get('/schedules/doctors-with-working-hours', verifyToken, verifyRole('Manager'), getDoctorsWithWorkingHours)
+router.put('/schedules/doctor/:doctorId/working-hours', verifyToken, verifyRole('Manager'), updateDoctorWorkingHours)
 
 // Working Hours routes
 router.get('/schedules/:scheduleId/working-hours', verifyToken, verifyRole('Manager'), getWorkingHours)
 router.put('/schedules/:scheduleId/working-hours', verifyToken, verifyRole('Manager'), updateWorkingHours)
 router.put('/schedules/doctor/:doctorId/date/:date/working-hours', verifyToken, verifyRole('Manager'), updateDoctorWorkingHoursForDate)
 
-// New routes for doctor working hours management
-router.get('/doctors-with-working-hours', verifyToken, verifyRole('Manager'), getDoctorsWithWorkingHours)
-router.put('/doctor/:doctorId/working-hours', verifyToken, verifyRole('Manager'), updateDoctorWorkingHours)
+// These routes with :id parameter MUST be last
+router.get('/schedules/:id', verifyToken, verifyRole('Manager'),viewDetailScheduleDoctor)
+router.patch('/schedules/:id', verifyToken, verifyRole('Manager'),updateScheduleDoctor)
+router.delete('/schedules/:id',verifyToken, verifyRole('Manager'), deleteSchedule)
 
 module.exports = router;
